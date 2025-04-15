50b0d6edf2bc09fa2f91552f13aad099c2885448
BOCz08 <121933217+BOCz08@users.noreply.github.com>
2025-04-15 19:52:51 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -36,7 +36,6 @@ PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-udpxy-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ddns-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
-
 PACKAGES="$PACKAGES openssh-sftp-server"
 # 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
